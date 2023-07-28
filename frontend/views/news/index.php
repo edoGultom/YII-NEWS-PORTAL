7fe54fc1c935d44e8ab377d0869c6a0a5f9a6c7b
Dwi Herydo Gultom <edogultom10395@gmail.com>
2023-07-28 08:09:04 +0700

    slug

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -21,7 +21,7 @@ $this->title = $title;
             <div class="tab active-tab" id="prod-alls">
                 <div class="content">
                     <div class="sec-title">
-                        <h2>All News</h2>
+                        <h2><?= $this->title ?></h2>
                     </div>
                     <div class="row clearfix">
                         <!-- News Block Two -->
@@ -40,36 +40,34 @@ $this->title = $title;
                             }
                             $isi = strip_tags($value->isi);
                         ?>
-                        <div class="news-block-two col-lg-6 col-md-6 col-sm-12">
-                            <div class="inner-box">
-                                <div class="single-item-carousel owl-carousel owl-theme">
-                                    <div class="image">
-                                        <!-- <img src="/images/resource/news-37.jpg" alt="" /> -->
-                                        <?= LazyLoad::widget([
+                            <div class="news-block-two col-lg-6 col-md-6 col-sm-12">
+                                <div class="inner-box">
+                                    <div class="single-item-carousel owl-carousel owl-theme">
+                                        <div class="image">
+                                            <!-- <img src="/images/resource/news-37.jpg" alt="" /> -->
+                                            <?= LazyLoad::widget([
                                                 'src' => Url::to(['/document/get-file', 'id' =>  $value->ambilgambar->id ?? NULL]),
                                             ]); ?>
-                                        <div class="category">
-                                            <a
-                                                href="<?= Url::to(['/news/' . $value->sub_judul]) ?>"><?= $value->kategoriArtikel->keterangan ?? '-' ?></a>
+                                            <div class="category">
+                                                <a href="<?= Url::to(['/news/' . $value->sub_judul]) ?>"><?= $value->kategoriArtikel->keterangan ?? '-' ?></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="lower-box">
-                                    <h3>
-                                        <a
-                                            href="<?= Url::to(['/news/' . $value->sub_judul]) ?>"><?= $value->judul ?></a>
-                                    </h3>
-                                    <ul class="post-meta">
-                                        <li><span
-                                                class="icon fa fa-clock-o"></span><?= date("d-m-Y", substr($value->created_at, 0, 10)) ?>
-                                        </li>
-                                    </ul>
-                                    <div class="text"><?= implode(' ', array_slice(explode(' ', $isi), 0, 35)) ?></div>
-                                    <a href="<?=Url::to(['/news/' . $value->sub_judul])?>" class="read-more">Read
-                                        More </a>
+                                    <div class="lower-box">
+                                        <h3>
+                                            <a href="<?= Url::to(['/news/' . $value->sub_judul]) ?>"><?= $value->judul ?></a>
+                                        </h3>
+                                        <ul class="post-meta">
+                                            <li><span class="icon fa fa-clock-o"></span><?= date("d-m-Y", substr($value->created_at, 0, 10)) ?>
+                                            </li>
+                                            <li><span class="icon fa fa-eye"></span><?= $value->jumlah_visit ?? 0 ?></li>
+                                        </ul>
+                                        <div class="text"><?= implode(' ', array_slice(explode(' ', $isi), 0, 25)) ?></div>
+                                        <a href="<?= Url::to(['/news/' . $value->sub_judul]) ?>" class="read-more">Read
+                                            More </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         <?php
                         }
                         ?>
